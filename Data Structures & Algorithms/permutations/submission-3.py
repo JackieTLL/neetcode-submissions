b735d8a3b44fc00296217fa898ce45013eb1a7f2
JackieTLL <122020014@link.cuhk.edu.cn>
2026-09-08 13:23:45 -0400

    Add: permutations - submission-3

---
+++ Data Structures & Algorithms/permutations/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if nums == []:
+            return [[]]
+        first = nums[0]
+        other = nums[1:]
+        return [oneDList[:i] + [first] + oneDList[i:]
+                for oneDList in self.permute(other) 
+                for i in range(len(oneDList) + 1)]
+        
