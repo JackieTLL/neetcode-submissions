4d15a7025507c1ded255e1c3690a35b70a23e859
JackieTLL <122020014@link.cuhk.edu.cn>
2026-09-20 18:15:04 -0400

    Add: longest-increasing-subsequence - submission-18

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-18.py
@@ -0,0 +1,26 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        memo = dict()
+        n = len(nums)
+        def solve(i):
+            if i in memo:
+                return memo[i]
+            if i >= n:
+                return 0
+            k = -1
+            best = float('-inf')
+            for j in range(i + 1, n):
+                if nums[j] > nums[i]:
+                    k = j
+                    solution = solve(k)
+                    best = max(best, 1 + solve(k))
+            if best == float('-inf'):
+                memo[i] = 1
+            else:
+                memo[i] = best
+            return memo[i]
+        best = 1
+        for i in range(n):
+            best = max(best, solve(i))
+            print(best, i)
+        return best
