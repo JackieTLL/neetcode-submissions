6ddc3e458f621a1c4e39a4f07323a5df9f52ea2b
JackieTLL <122020014@link.cuhk.edu.cn>
2026-09-20 17:40:13 -0400

    Add: coin-change - submission-12

---
+++ Data Structures & Algorithms/coin-change/submission-12.py
@@ -0,0 +1,23 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        memo = dict()
+        def solve(amount):
+            if amount in memo:
+                return memo[amount]
+            if amount < 0:
+                return -1
+            if amount == 0:
+                return 0
+            best = float('inf')
+            for coin in coins:
+                if coin <= amount:
+                    solution = solve(amount - coin)
+                    if solution != -1:
+                        best = min(best, 1 + solution)
+                if best == float('inf'):
+                    memo[amount] = -1
+                else:
+                    memo[amount] = best
+            return memo[amount]
+        return solve(amount)
+        
